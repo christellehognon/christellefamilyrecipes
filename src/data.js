3eac267e74c3645f8eb9c771d0d3c68b3a0cb00f
Christelle <christelle.hognon@fasst.io>
2024-04-27 23:24:17 +0200

    add image

--- src/data.js
+++ src/data.js
@@ -86,11 +86,11 @@ export const recipes = [{
     timeToCook: "30 minutes + 25 minutes de cuisson",
     averagePrice: "€",
     difficulty: "Facile",
-    ingredients: ["75g de farine", "1 cuillère à café rase de canelle", "1 pincée de sel", "25g de sucre en poudre", "50g de beurre ramolli", "4 grosses pommes", "100g de sucre", "eau"],
-    description: "Pour le granité, mélanger à la fourchette pour être bien fluide (pas de grumeaux).La farine, le sucre puis le beurre et le sucre vanillé. Faire un sable. Couper les pommes en tranches fines. Les faire sauter dans une poêle dans un sirop de sucre (eau et 100g de sucre). Les faire revenir rapidement. Laisser refroidir. Mettre les pommes dans un moule de 26cm environ, et répendre le sable. Mettre au four thermostat 7 environ 25’. Servir tiède avec une crème pâtissière très froide."
+    ingredients: ["75g de farine", "1 cuillère à café rase de canelle", "1 pincée de sel", "25g de sucre en poudre", "50g de beurre ramolli", "4 grosses pommes", "50g de sucre", "50g d'eau"],
+    description: "Pour le granité, mélanger à la fourchette pour être bien fluide (pas de grumeaux) la farine, le sucre puis le beurre la canelle et le sucre vanillé. Faire un sable. Couper les pommes en tranches fines. Les faire sauter dans une poêle dans un sirop de sucre (eau et 100g de sucre). Les faire revenir rapidement. Laisser refroidir. Mettre les pommes dans un moule de 26 cm environ et répendre le sable. Mettre au four à 210°C environ 25’. Servir tiède avec une crème pâtissière très froide."
 },
 {
-    image: "",
+    image: "/img/top-chiefs/img_37.jpg",
     authorImg: "/img/top-chiefs/img_1.jpg",
     name: "Crème pâtissière",
     slug: "creme-patissiere",
